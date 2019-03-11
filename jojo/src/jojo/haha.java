1441cec8a089d1a1f9c6726c404ac5cedf330676
sk-2 <2286729934@qq.com>
2019-03-11 19:45:25 +0800

    上传一个项目到远端仓库

---
+++ jojo/src/jojo/haha.java
@@ -0,0 +1,24 @@
+package jojo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+class haha {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
